581770eaa5db2b453c979528d89a5050eb2a28fd
mschoenebeck <matthias.schoenebeck@gmail.com>
2023-09-22 11:49:57 -0500

    added fpie linker flag and include guards for nonstd span

--- include/span.h
+++ include/span.h
@@ -3,10 +3,13 @@
 #ifdef __cpp_lib_span
 #   include <span>
 #else
+#ifndef NONSTDSPAN
+#define NONSTDSPAN
 #   include <limits>
 #   include "nonstd/span.hpp"
 namespace std {
 template <class T, std::size_t Extent= std::numeric_limits<std::size_t>::max()>
 using span = nonstd::span<T, Extent>;
 } // namespace std
+#endif
 #endif
